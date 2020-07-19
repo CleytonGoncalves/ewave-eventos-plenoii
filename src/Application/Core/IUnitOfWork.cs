e6220cf0bf50b1d169a0bc96c42dabe4d8cd24ae
Cleyton <cleyton@cleytongoncalves.com>
2020-07-18 23:11:08 -0400

    Cria unit of work

---
+++ src/Application/Core/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public interface IUnitOfWork
+    {
+        /// <summary> Concretiza todas as mudanças de uma vez só (database changes) </summary>
+        /// <returns>Quantidade de linhas afetadas</returns>
+        Task<int> Commit(CancellationToken cToken = default);
+    }
+}
---
+++ src/Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+
+namespace Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly PalestraContext _context;
+        private bool _disposed;
+
+        public UnitOfWork(PalestraContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Commit(CancellationToken cToken = default)
+        {
+            int affectedRows = await _context
+                .SaveChangesAsync(cToken)
+                .ConfigureAwait(false);
+
+            return affectedRows;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (! _disposed && disposing)
+                _context.Dispose();
+
+            _disposed = true;
+        }
+    }
+}
