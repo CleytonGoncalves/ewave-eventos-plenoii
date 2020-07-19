cd9b186e71ea0026498a5d6678db8d97703c9f17
Cleyton <cleyton@cleytongoncalves.com>
2020-07-19 19:27:04 -0400

    Corrige Unit of Work dando dispose no DbContext
    Não é necessário, pois o DbContext está em um DbContextPool

--- src/Infrastructure/Data/UnitOfWork.cs
+++ src/Infrastructure/Data/UnitOfWork.cs
@@ -6,12 +6,10 @@ using Infrastructure.Processing;
 
 namespace Infrastructure.Data
 {
-    public class UnitOfWork : IUnitOfWork, IDisposable
+    public class UnitOfWork : IUnitOfWork
     {
         private readonly PalestraContext _context;
         private readonly IDomainEventDispatcher _eventDispatcher;
-
-        private bool _disposed;
 
         public UnitOfWork(PalestraContext context, IDomainEventDispatcher eventDispatcher)
         {
@@ -26,20 +24,6 @@ namespace Infrastructure.Data
             int affectedRows = _context.SaveChanges();
 
             return affectedRows;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (! _disposed && disposing)
-                _context.Dispose();
-
-            _disposed = true;
         }
     }
 }
