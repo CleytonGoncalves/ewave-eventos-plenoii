fcae8c64b916a0c921e44338e8f9300c5c6c32f5
Cleyton <cleyton@cleytongoncalves.com>
2020-07-19 00:50:38 -0400

    Cria core dos testes de unidade

---
+++ tests/UnitTests/Core/AggregateAssertions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Domain.Core;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace UnitTests.Core
+{
+    public class AggregateAssertions : ReferenceTypeAssertions<EntityBase, AggregateAssertions>
+    {
+        protected override string Identifier => "Aggregate";
+
+        public AggregateAssertions(EntityBase aggregate) : base(aggregate)
+        {
+        }
+
+        public AndConstraint<AggregateAssertions> HavePublishedEventOf<T>()
+        {
+            var domainEvent = Subject.GetAllDomainEvents().OfType<T>().FirstOrDefault();
+
+            domainEvent.Should().BeOfType<T>();
+
+            return new AndConstraint<AggregateAssertions>(this);
+        }
+
+        public AndConstraint<AggregateAssertions> BreakBusinessRule<TRule>(Action action, string because = "",
+            params object[] becauseArgs)
+            where TRule : class, IBusinessRule
+        {
+            action.Should()
+                .Throw<BusinessRuleValidationException>(because, becauseArgs)
+                .Where(x => x.BrokenRule is TRule);
+
+            return new AndConstraint<AggregateAssertions>(this);
+        }
+    }
+}
---
+++ tests/UnitTests/Core/AggregateExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Domain.Core;
+
+namespace UnitTests.Core
+{
+    public static class AggregateExtensions
+    {
+        public static AggregateAssertions Should(this EntityBase instance) => new AggregateAssertions(instance);
+
+        public static List<IDomainEvent> GetAllDomainEvents(this EntityBase aggregate)
+        {
+            var domainEvents = new List<IDomainEvent>();
+
+            if (aggregate.DomainEvents.Any())
+                domainEvents.AddRange(aggregate.DomainEvents);
+
+            ExecuteActionOnSubAggregateEvents(aggregate, (subAggregates) =>
+                domainEvents.AddRange(GetAllDomainEvents(subAggregates)));
+
+            return domainEvents;
+        }
+
+        public static void ClearAllDomainEvents(this EntityBase aggregate)
+        {
+            aggregate.ClearDomainEvents();
+            ExecuteActionOnSubAggregateEvents(aggregate, ClearAllDomainEvents);
+        }
+
+        private static void ExecuteActionOnSubAggregateEvents(EntityBase aggregate, Action<EntityBase> action)
+        {
+            var fields = aggregate.GetType()
+                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
+                .Concat(aggregate.GetType().BaseType!.GetFields(BindingFlags.NonPublic | BindingFlags.Instance |
+                    BindingFlags.Public))
+                .ToArray();
+
+            foreach (var field in fields)
+            {
+                bool isEntity = field.FieldType.IsAssignableFrom(typeof(EntityBase));
+
+                if (isEntity)
+                {
+                    var entity = field.GetValue(aggregate) as EntityBase;
+                    action(entity);
+                }
+
+                if (field.FieldType == typeof(string) || ! typeof(IEnumerable).IsAssignableFrom(field.FieldType))
+                    continue;
+
+                if (field.GetValue(aggregate) is IEnumerable enumerable)
+                {
+                    foreach (var en in enumerable)
+                    {
+                        if (en is EntityBase entityItem)
+                            action(entityItem);
+                    }
+                }
+            }
+        }
+    }
+}
